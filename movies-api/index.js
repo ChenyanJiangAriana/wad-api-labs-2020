e1a56ada7886100c95e444854d56d918cf5ead13
Chenyan <624071310@qq.com>
2020-12-27 23:41:56 +0000

    Week 9 Error HAndling

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,18 +7,30 @@ dotenv.config();
 
 const app = express();
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT;
 //Add the bodyparser.json() and bodypsrser.urlencoded() 
 //to the app's middleware stack. 
 //The parsers must be put into the code before the moviesRouter. 
 //Remember Express executes the middleware stack in the order that is appears in the code.
 //configure body-parser
+// eslint-disable-next-line no-undef
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());//step to add post(Add movies)_
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
-
+app.use(errHandler);
 
 
 app.listen(port, () => {

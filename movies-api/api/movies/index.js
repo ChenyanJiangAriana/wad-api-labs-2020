4aba29510f16f8c90d7bf599c76d5c31f377b4ee
Chenyan <624071310@qq.com>
2020-12-28 20:54:28 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,21 +1,25 @@
 import express from 'express';
 import {
-  getMovies, getMovie, getMovieReviews
+   getMovie, getMovieReviews
 } from '../tmdb-api';
+import movieModel from './movieModel';
 
 const router = express.Router();
-
-router.get('/', (req, res,next) => {
-  getMovies().then(movies => res.status(200).send(movies))
-  .catch((error) => next(error));
+ 
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
+  movieModel.find().then(movies => res.status(200).send(movies)).catch(next);
 });
 
+// eslint-disable-next-line no-unused-vars
 router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   getMovie(id).then(movie => res.status(200).send(movie))
   .catch((error) => next(error));
+    
 });
 
+// eslint-disable-next-line no-unused-vars
 router.get('/:id/reviews', (req, res, next) => {
   const id = parseInt(req.params.id);
   getMovieReviews(id)
--- movies-api/index.js
+++ movies-api/index.js
@@ -1,59 +1,60 @@
-import './db';
-import session from 'express-session';
-import passport from './authenticate';
-import {loadUsers} from './seedData'
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import {loadUsers, loadMovies} from './seedData';
+import genresRouter from './api/genres';//exercise
 import bodyParser from 'body-parser';
+import './db';
 import usersRouter from './api/users';
+import session from 'express-session';
+import passport from './authenticate';
 
 
-//use body-parser in it's middleware stack. 
 dotenv.config();
 
 const app = express();
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT;
-//Add the bodyparser.json() and bodypsrser.urlencoded() 
-//to the app's middleware stack. 
-//The parsers must be put into the code before the moviesRouter. 
-//Remember Express executes the middleware stack in the order that is appears in the code.
-//configure body-parser
-// eslint-disable-next-line no-undef
+
+// eslint-disable-next-line no-unused-vars
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
+  // eslint-disable-next-line no-undef
   if(process.env.NODE_ENV === 'production') {
     return res.status(500).send(`Something went wrong!`);
   }
   res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
 };
 
+
+// eslint-disable-next-line no-undef
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();
 }
+
+//session middleware
 app.use(session({
   secret: 'ilikecake',
   resave: true,
   saveUninitialized: true
 }));
-// initialise passport​
+
 app.use(passport.initialize());
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());//step to add post(Add movies)_
-
+app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
-app.use('/api/movies', moviesRouter);
-//Users router
-app.use('/api/users', usersRouter);
 
-// Add passport.authenticate(..)  to middleware stack for protected routes​
 app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
+app.use('/api/genres',genresRouter);//exercise
+//Users router
+app.use('/api/users', usersRouter);
 app.use(errHandler);
-
+//update /api/Movie route
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
